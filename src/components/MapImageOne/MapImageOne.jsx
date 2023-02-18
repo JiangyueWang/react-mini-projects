b8be6e10ca8295b2c9970fe8481354073892434c
Jiang <jiangyue0380@gmail.com>
2023-02-18 15:44:28 +1100

    updated MapImageOne component comment and resource

--- src/components/MapImageOne/MapImageOne.jsx
+++ src/components/MapImageOne/MapImageOne.jsx
@@ -1,14 +1,16 @@
 import { React,useState} from 'react';
 const MapImageOne = (props) => {
+    // initialise the current state variable 
     const [current, setCurrent] = useState(0);
     
+    // nextImg and preImg functions update current variable
     const nextImg = () => { 
         setCurrent(current === props.images.length - 1 ? 0 : current + 1)
     }
-
     const preImg = () => {
         setCurrent(current === 0 ? props.images.length - 1 : current - 1)
     }
+
     return (  
         <div>            
             <p onClick={nextImg}> 
@@ -24,8 +26,20 @@ const MapImageOne = (props) => {
             )}
 
             <p onClick={preImg}>
-                previous
+                Previous
             </p> 
+
+            <div>
+                <p>
+                    React concepts used
+                </p> 
+                <ul>
+                    <li>useState (storing and updating state)</li>
+                    <li>Conditionals (ternaries)</li>
+                    <li>Lists, keys, and .map()</li>
+                </ul>
+                <p>Resource: <a href="https://www.freecodecamp.org/news/react-projects-for-beginners-easy-ideas-with-code/">React Projects for Beginners in 2023 – Fun Ideas with Code</a></p>
+            </div>
         </div>
 
     );
